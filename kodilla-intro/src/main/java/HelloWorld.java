5fd6e14caa82b3cf7097c18d78d7bfb52c9345d3
Michal Marcol <michmarcol@gmail.com>
2020-05-02 19:52:48 +0200

    First

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main (String []args)
+    {
+        System.out.println("Hello world!\n" + "Moje imię: Michał\nMój wiek: " + 24 +"\nPierwsza litera nazwiska to: M");
+    }
+}
