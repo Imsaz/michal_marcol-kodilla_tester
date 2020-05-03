d7ba1ab6258ba649573eee8bf50805773b12d268
Michal Marcol <michmarcol@gmail.com>
2020-05-03 08:49:05 +0200

    second exercises

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended
+{
+    public static void main (String[] args)
+    {
+        boolean trueOrFalse = true;
+        String carCompany = "Ford";
+        int ageOfCar = 24;
+        double petrolUsage = 7.6;
+        char carClass = 'C';
+        boolean buy = false;
+
+        System.out.println(trueOrFalse + "\n" + carCompany + "\n" + ageOfCar + "\n" + petrolUsage + "\n" + carClass + "\n" + buy);
+    }
+
+}
