fe545949c752b6b7cc789504f96ac483899bc404
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-05-14 22:32:40 +0200

    2.3 Review

--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,64 +1,43 @@
 public class Notebook {
 
-    String weight;
+    String weight;//TODO Review pls use int here
     int price;
     int year;
 
-    public Notebook (String weight, int price, int year)
-    {
+    public Notebook(String weight, int price, int year) {
         this.weight = weight;
         this.price = price;
         this.year = year;
     }
 
-    public void checkPrice()
-    {
-        if (this.price < 600)
-        {
+    public void checkPrice() {
+        if (this.price < 600) {
             System.out.println("This notebook is very cheap.");
-        }
-        else if (this.price >= 600 && this.price <= 1000)
-        {
+        } else if (this.price >= 600 && this.price <= 1000) {
             System.out.println("This price is good");
-        }
-        else
-        {
+        } else {
             System.out.println("This notebook is expensive");
         }
     }
 
-    public void checkWeight()
-    {
-        if (this.weight == "600g")
-        {
+    public void checkWeight() {
+        if (this.weight == "600g") {
             System.out.println("This device is light");
-        }
-        else if (this.weight == "1500g")
-        {
+        } else if (this.weight == "1500g") {
             System.out.println("This device is not too heavy");
-        }
-        else
-        {
+        } else {
             System.out.println("This device is very heavy");
         }
     }
 
-    public void checkYearAndPrice()
-    {
-        if (this.year >= 2015 && this.price < 1500)
-        {
+    public void checkYearAndPrice() {
+        if (this.year >= 2015 && this.price < 1500) {
             System.out.println("This notebook is a good choice");
-        }
-        else if(this.year <= 2015 && this.price < 1500)
-        {
+        } else if (this.year <= 2015 && this.price < 1500) {
             System.out.println("This notebook has a good price, but it is little old");
-        }
-        else if (this.year >= 2015 && this.price >1500)
-        {
+        } else if (this.year >= 2015 && this.price > 1500) {
             System.out.println("This notebook is not a good choice");
-        }
-        else
-        {
+        } else {
             System.out.println("This notebook is a misunderstanding");
         }
     }
