b712ba60851d6ba18d81415ebf75b23393473168
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-05-18 21:32:41 +0200

    2.8 Review

--- kodilla-intro/src/main/java/colorChoice.java
+++ kodilla-intro/src/main/java/colorChoice.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
-public class colorChoice {
+public class colorChoice {/*TODO Review: Class names should start with capital letter */
 
-    public static String getFirstLetter(){
+    public static String getFirstLetter() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Select calculation: R - red, B - blue, Y - yellow, G - green:");
             String calc = scanner.nextLine().trim().toUpperCase();
-            switch(calc) {
-                case "R" : return "RED";
-                case "B" : return "BLUE";
-                case "Y" : return "YELLOW";
-                case "G" : return "GREEN";
+            switch (calc) {
+                case "R":
+                    return "RED";
+                case "B":
+                    return "BLUE";
+                case "Y":
+                    return "YELLOW";
+                case "G":
+                    return "GREEN";
                 default:
                     System.out.println("Wrong letter. Try again.");
             }
         }
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println(getFirstLetter());
     }
 
