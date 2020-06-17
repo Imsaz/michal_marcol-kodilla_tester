6b31fe68c96562866d5684bd904892ba6ab04d53
Michal Marcol <michmarcol@gmail.com>
2020-06-17 20:49:41 +0200

    7.1. Problem with tests

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/NumberCheckerTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NumberCheckerTestSuite {
+
+    private NumberChecker numberChecker = new NumberChecker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3, 6, -3, -12, 15})
+    public void shouldReturnTrueForNumberDivisibleByThree(int number) {
+        boolean result = numberChecker.isDivisibleByThree(number);
+        assertTrue(result);
+    }
+}
