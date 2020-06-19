8e12157d7c392c2286d78f5f8e7632fbf740ca8a
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-06-19 07:16:59 +0200

    6.3. Stream – ForumStats

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,28 +1,42 @@
 package com.kodilla.stream.homework;
 
+import com.kodilla.stream.User;
 import com.kodilla.stream.UsersRepository;
 
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
-//TODO
-
 public class ForumStats {
     public static void main(String[] args) {
-        var postList = UsersRepository.getUsersLists()
+        var avgNumberOfPostsBy40Plus = UsersRepository.getUsersLists()
                 .stream()
-                .map(u -> u.getAge())
-                //.mapToInt(n -> n)
-                .filter(a -> a >= 40)
+                .map(User::getAge)
+                .filter(age -> age >= 40)
+                .map(ForumStats::getUserByAge)
+                .map(User::getNumberOfPosts)
+                .mapToInt(postsNumber -> postsNumber)
+                .average()
+                .getAsDouble();
 
-                .collect(toList());
-        //.average()
-        //.getAsDouble();
-        System.out.println(postList);
+        var avgNumberOfPostsByBelow40 = UsersRepository.getUsersLists()
+                .stream()
+                .map(User::getAge)
+                .filter(age -> age < 40)
+                .map(ForumStats::getUserByAge)
+                .map(User::getNumberOfPosts)
+                .mapToInt(postsNumber -> postsNumber)
+                .average()
+                .getAsDouble();
 
-        //UsersRepository.getUsersLists().
+        System.out.println(avgNumberOfPostsBy40Plus);
+        System.out.println(avgNumberOfPostsByBelow40);
     }
 
-    private static
+    private static User getUserByAge(int age) {
+        return UsersRepository.getUsersLists()
+                .stream().filter(u -> u.getAge() == age)
+                .findFirst()
+                .get();
+    }
 }
