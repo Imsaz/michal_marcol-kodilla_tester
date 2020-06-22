608a4f865bb7f44b853b3094828434f0df9bad8e
Michal Marcol <michmarcol@gmail.com>
2020-06-22 16:15:48 +0200

    6.3. exercise ForumStats

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -26,5 +26,44 @@ public class ForumStats {
 
 
     }
-
 }
+
+/*
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.*;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        var avgNumberOfPostsBy40Plus = UsersRepository.getUsersLists()
+                .stream()
+                .map(User::getAge)
+                .filter(age -> age >= 40)
+                .map(ForumStats::getUserByAge)
+                .map(User::getNumberOfPosts)
+                .mapToInt(postsNumber -> postsNumber)
+                .average()
+                .getAsDouble();
+
+        var avgNumberOfPostsByBelow40 = UsersRepository.getUsersLists()
+                .stream()
+                .map(User::getAge)
+                .filter(age -> age < 40)
+                .map(ForumStats::getUserByAge)
+                .map(User::getNumberOfPosts)
+                .mapToInt(postsNumber -> postsNumber)
+                .average()
+                .getAsDouble();
+
+        System.out.println(avgNumberOfPostsBy40Plus);
+        System.out.println(avgNumberOfPostsByBelow40);
+    }
+
+    private static User getUserByAge(int age) {
+        return UsersRepository.getUsersLists()
+                .stream().filter(u -> u.getAge() == age)
+                .findFirst()
+                .get();
+    }
+>>>>>>> origin/master
+}
+*/
