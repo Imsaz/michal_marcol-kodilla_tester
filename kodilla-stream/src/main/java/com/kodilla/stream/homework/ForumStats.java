1ffaf0c7be9f061305754529cb793fde62da1231
Michal Marcol <michmarcol@gmail.com>
2020-06-22 16:10:48 +0200

    6.3. exercise ForumStats

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,28 +1,30 @@
 package com.kodilla.stream.homework;
 
+import com.kodilla.stream.User;
 import com.kodilla.stream.UsersRepository;
-
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.*;
-
-//TODO
 
 public class ForumStats {
     public static void main(String[] args) {
-        var postList = UsersRepository.getUsersLists()
+        var avgPostList = UsersRepository.getUsersLists()
                 .stream()
-                .map(u -> u.getAge())
-                //.mapToInt(n -> n)
-                .filter(a -> a >= 40)
+                .filter(example -> example.getAge() >= 40)
+                .map(User::getNumberOfPosts)
+                .mapToInt(changeStream -> changeStream)
+                .average()
+                .getAsDouble();
 
-                .collect(toList());
-        //.average()
-        //.getAsDouble();
-        System.out.println(postList);
+        var avgPostListUnderForty = UsersRepository.getUsersLists()
+                .stream()
+                .filter(example -> example.getAge() < 40)
+                .map(User::getNumberOfPosts)
+                .mapToInt(changeStream -> changeStream)
+                .average()
+                .getAsDouble();
 
-        //UsersRepository.getUsersLists().
+        System.out.println(avgPostList);
+        System.out.println(avgPostListUnderForty);
+
+
     }
 
-    private static
 }
