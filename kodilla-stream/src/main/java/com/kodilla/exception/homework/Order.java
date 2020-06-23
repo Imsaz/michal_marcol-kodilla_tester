6303336fb5fb905a7e816e9a97ef9606b8a7cc32
Michal Marcol <michmarcol@gmail.com>
2020-06-23 20:27:24 +0200

    6.5. Exemptions

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Order.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Order.java
@@ -1,7 +1,22 @@
 package com.kodilla.exception.homework;
 
+import java.util.Objects;
+
 public class Order {
     String number;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return Objects.equals(number, order.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 
     @Override
     public String toString() {
--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.exception.homework;
 
-import org.junit.Assert;
+import com.kodilla.exception.AirportNotFoundException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,10 +10,34 @@ import static org.junit.Assert.*;
 public class WarehouseTestSuite {
 
     @Test
-    public void testFailed(){
-        Assert.fail("not implemented yet");
+    public void testGetOrderWithoutExemption() throws OrderDoesntExistException {
+        // given
+        Warehouse warehouse = new Warehouse();
+
+        Order order1 = new Order("111");
+        Order order2 = new Order("222");
+        Order order3 = new Order("333");
+
+        warehouse.addOrder(order1);
+        warehouse.addOrder(order2);
+        warehouse.addOrder(order3);
+        // when
+        boolean orderExist = new Order("222").equals(warehouse.getOrder("222"));
+        // then
+        assertTrue(orderExist);
     }
 
-    //@Test
+    @Test (expected = OrderDoesntExistException.class)
+    public void testGetOrder_WithExemption() throws OrderDoesntExistException {
+        // given
+        Warehouse warehouse = new Warehouse();
 
+        warehouse.addOrder(new Order("111"));
+        warehouse.addOrder(new Order("222"));
+        warehouse.addOrder(new Order("333"));
+        // when
+        boolean orderDoesntExist = new Order("222").equals(warehouse.getOrder("22"));
+        // then
+        assertTrue(orderDoesntExist);
+    }
 }
