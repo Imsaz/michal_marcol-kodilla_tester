ed929760d81dbb22289a74986e19929ba246eacc
Michal Marcol <michmarcol@gmail.com>
2020-08-15 13:05:45 +0200

    13.4. XPath

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -13,17 +13,20 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
 
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+        Thread.sleep(2000);
+        //WebElement closeField = driver.findElement(By.xpath("//html/body/div/div/div/div/div/button"));
+        WebElement closeField = driver.findElement(By.xpath("//*[@type=\"button\" and @data-role=\"close-and-accept-consent\"]"));
+        closeField.click();
 
-        //WebElement inputField = driver.findElement(By.name("string"));
-        //inputField.sendKeys("Mavic mini");
+        //closeField = driver.findElement(By.xpath("//html/body/div[2]/div[2]/header/div/div/div/div/form/div[2]/div/select"));
+        closeField = driver.findElement(By.xpath("//select[contains(@class,\"mr3m_1 m7er_k4\") and contains(@data-role,\"filters-dropdown\") and contains(@aria-label,\"Kategoria\")]"));
+        Select categorySelect = new Select(closeField);
+        categorySelect.selectByIndex(3);
+        Thread.sleep(2000);
 
-        //Alert alert = driver.switchTo().alert();
-        //alert.accept();
-
-        //WebElement typeThing = driver.findElement(By.xpath("//*[contains(@aria-label=\"Kategorie i opcje\"]/optgroup)"));
-       // Select categorySelect = new Select(typeThing);
-       // categorySelect.selectByIndex(0);
+        //closeField = driver.findElement(By.xpath("//html/body/div[2]/div[2]/header/div/div/div/div/form/input"));
+        closeField = driver.findElement(By.xpath("//input[@type=\"search\" and contains(@autocomplete, \"f\") and @name=\"string\" and @role=\"combobox\"]"));
+        closeField.sendKeys("Mavic mini");
+        closeField.submit();
     }
 }
